70323e7a9825e6db919dff78fdfb31dc43884d07
leomiglio02 <leo@miglio.dev>
2022-11-29 19:19:25 +0100

    add property lavora in

--- api/src/main/java/it/unicam/ids/studenti/ll/model/Dipendente.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/Dipendente.java
@@ -1,24 +1,24 @@
 package it.unicam.ids.studenti.ll.model;
 
 abstract class Dipendente extends Persona {
-	public int identificativo;
-	private String password;
+    public int identificativo;
+    private String password;
+	public Azienda lavoraIn;
 
-	public int getIdentificativo() {
-		return this.identificativo;
-	}
+    public int getIdentificativo() {
+        return this.identificativo;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	/**
-	 * 
-	 * @param password
-	 */
-	public boolean isPasswordValid(String password) {
-		// TODO - implement Dipendente.isPasswordValid
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * @param password
+     */
+    public boolean isPasswordValid(String password) {
+        // TODO - implement Dipendente.isPasswordValid
+        throw new UnsupportedOperationException();
+    }
 
 }
