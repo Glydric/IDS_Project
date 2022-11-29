deffcf2a2e33181ddbe0f278e1607938c1b24147
leomiglio02 <leo@miglio.dev>
2022-11-29 17:29:03 +0100

    removed useless

--- api/src/main/java/it/unicam/ids/studenti/ll/model/Persona.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/Persona.java
@@ -6,23 +6,7 @@ class Persona {
 
 	final public Date dataNascita = new Date();
 	public String nome;
-	private String cognome;
-
-	public String getNome() {
-		return this.nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCognome() {
-		return this.cognome;
-	}
-
-	public void setCognome(String cognome) {
-		this.cognome = cognome;
-	}
+	public String cognome;
 
 	short getEta() {
 		// TODO - implement Persona.getEta
