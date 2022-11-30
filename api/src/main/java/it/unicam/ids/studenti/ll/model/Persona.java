5dafc2762f81ab770fcce51ee611c98ca77fc293
CessoFlesso <82220224+CessoFlesso@users.noreply.github.com>
2022-11-30 18:41:25 +0100

    Update Persona.java
    le variabili interne alla classe impostate a private.
    creazione classe persona,cliente.

--- api/src/main/java/it/unicam/ids/studenti/ll/model/Persona.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/Persona.java
@@ -1,21 +1,23 @@
 package it.unicam.ids.studenti.ll.model;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 class Persona {
+    private LocalDate dataNascita;
+    private String nome;
+    private String cognome;
 
-    final public Date dataNascita = new Date();
-    public String nome;
-    public String cognome;
-
-    public Persona(String nome, String cognome) {
+    public Persona(String nome, String cognome, LocalDate dataNascita) {
         this.nome = nome;
         this.cognome = cognome;
+        this.dataNascita = dataNascita;
     }
 
     short getEta() {
-        // TODO - implement Persona.getEta
-        throw new UnsupportedOperationException();
+    LocalDate oggi = LocalDate.now(); //ho istanziato il giorno corrente per poterlo usare.
+    int eta = oggi.getYear() - this.dataNascita.getYear();// qui uso per rtovare l'eta'.
+    return (short) eta;
     }
 
 }
