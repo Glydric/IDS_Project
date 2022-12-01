8f457e1bb78db2b8588e008a313e3bbc278a0ad2
leomiglio02 <leo@miglio.dev>
2022-12-01 17:37:44 +0100

    [add] listacommercianti

--- api/src/main/java/it/unicam/ids/studenti/ll/model/Cliente.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/Cliente.java
@@ -1,10 +1,13 @@
 package it.unicam.ids.studenti.ll.model;
 
+import java.util.*;
+
 public class Cliente extends Persona {
     public String numeroTelefono;
     public boolean isFamily = false;
     private int identificativoTessera;
     private String email;
+	public List<Commerciante> listaCommercianti;
 
     public Cliente(String nome, String cognome) {
         super(nome, cognome);
