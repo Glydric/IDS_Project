715e1b7f2ba7b7b18830afedc6e0d87ba29adfb4
leomiglio02 <leo@miglio.dev>
2022-12-02 16:37:59 +0100

    magazzino e prodotto non più interessanti

--- api/src/main/java/it/unicam/ids/studenti/ll/model/ProgrammaCashback.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/ProgrammaCashback.java
@@ -1,4 +1,11 @@
 package it.unicam.ids.studenti.ll.model;
 
+import java.util.function.BiConsumer;
+
 public class ProgrammaCashback implements ProgrammaFedelta {
+    @Override
+    public BiConsumer<ProgrammaFedelta, ProgrammaFedelta> getDefaultConsumer() {
+        return null;		//TODO
+
+    }
 }
