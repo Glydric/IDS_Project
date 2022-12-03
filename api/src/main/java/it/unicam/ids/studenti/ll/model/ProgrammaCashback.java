b8527d4dd3655d5deb8ea5d94b2fba5ed16500f5
leomiglio02 <leo@miglio.dev>
2022-12-03 11:22:19 +0100

    [first implementation]

--- api/src/main/java/it/unicam/ids/studenti/ll/model/ProgrammaCashback.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/ProgrammaCashback.java
@@ -1,17 +1,56 @@
 package it.unicam.ids.studenti.ll.model;
 
+import java.util.Objects;
 import java.util.function.BiConsumer;
 
 public class ProgrammaCashback implements ProgrammaFedelta {
+    private float cashback = 0;
+
+    public ProgrammaCashback() {}
+
+    public ProgrammaCashback(float initCashback) {
+        cashback = initCashback;
+    }
+
+    public void addCashback(float add) {
+        if (add < 0) throw new IllegalArgumentException("Valore add negativo!!");
+        cashback += add;
+    }
+
+    public float getCashback() {
+        return cashback;
+    }
+
+    public float resetCashback() {
+        float oldCashback = cashback;
+        cashback = 0;
+        return oldCashback;
+    }
+
     @Override
     public ProgrammaFedelta clone() {
-        return new ProgrammaCashback();
+        return new ProgrammaCashback(cashback);
     }
 
     @Override
     public BiConsumer<ProgrammaFedelta, ProgrammaFedelta> getDefaultConsumer() {
-        return null;		//TODO
+        return (pr1, pr2) -> {
+            ProgrammaCashback p1 = (ProgrammaCashback) pr1;
+            ProgrammaCashback p2 = (ProgrammaCashback) pr2;
+            p1.addCashback(p2.getCashback());
+        };
 
     }
-    //TODO equals & hashcode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProgrammaCashback that)) return false;
+        return Float.compare(that.getCashback(), getCashback()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCashback());
+    }
 }
