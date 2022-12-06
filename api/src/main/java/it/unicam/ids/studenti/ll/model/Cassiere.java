2f02bd389c9ad4f35b065b893bd0f397a3d0728a
leomiglio02 <leo@miglio.dev>
2022-12-06 17:51:07 +0100

    [resolved] merge problems

--- api/src/main/java/it/unicam/ids/studenti/ll/model/Cassiere.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/Cassiere.java
@@ -1,7 +1,9 @@
 package it.unicam.ids.studenti.ll.model;
 
+import java.time.LocalDate;
+
 public class Cassiere extends Dipendente {
-    public Cassiere(String nome, String cognome) {
-        super(nome, cognome);
+    public Cassiere(String nome, String cognome, LocalDate dataNascita) {
+        super(nome, cognome, dataNascita);
     }
 }
--- api/src/main/java/it/unicam/ids/studenti/ll/model/Tecnico.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/Tecnico.java
@@ -1,8 +1,9 @@
-
 package it.unicam.ids.studenti.ll.model;
 
+import java.time.LocalDate;
+
 public class Tecnico extends Dipendente {
-    public Tecnico(String nome, String cognome) {
-        super(nome, cognome);
+    public Tecnico(String nome, String cognome, LocalDate dataNascita) {
+        super(nome, cognome, dataNascita);
     }
 }
