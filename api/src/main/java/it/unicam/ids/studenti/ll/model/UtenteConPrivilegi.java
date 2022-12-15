4a6f661c2edcc99f08222c506f2ead8477dbc8ee
Glydric22 <leo@miglio.dev>
2022-12-15 12:00:18 +0100

    [aggiunta] la possibilità di inserire permessi

--- api/src/main/java/it/unicam/ids/studenti/ll/model/UtenteConPrivilegi.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/UtenteConPrivilegi.java
@@ -1,21 +1,46 @@
 package it.unicam.ids.studenti.ll.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public abstract class UtenteConPrivilegi extends Persona {
-    private final Identificatore identificativo;
+    private final Identificatore identificativo = Identificatore.fromString(super.nome + "." + super.cognome);
+    public Set<Permesso> listaPermessi = new HashSet<>();
     private String password;
 
-    public UtenteConPrivilegi(String nome, String cognome, String identificativo, LocalDate dataNascita) {
-        super(nome, cognome, dataNascita);
-        this.identificativo = Identificatore.fromString(identificativo);
+    /**
+     * comodity method
+     *
+     * @param nome    il nome del dipendente
+     * @param cognome il cognome del dipendente
+     */
+    protected UtenteConPrivilegi(String nome, String cognome) {
+        super(nome, cognome);
     }
 
+    /**
+     * un costruttore che crea un utente con ID, in cui l'ID è formato da nome.cognome
+     *
+     * @param nome        il nome del dipendente
+     * @param cognome     il cognome del dipendente
+     * @param dataNascita la data di nascita
+     */
     public UtenteConPrivilegi(String nome, String cognome, LocalDate dataNascita) {
-        this(nome, cognome, nome + "." + cognome, dataNascita);
+        super(nome, cognome, dataNascita);
     }
 
+    public UtenteConPrivilegi(String nome, String cognome, String identificativo, LocalDate dataNascita) {
+        this(nome, cognome, dataNascita);
+        this.identificativo.updateIdentificativo(identificativo);
+    }
+
+    public void addPermesso(Permesso permesso) {
+        listaPermessi.add(permesso);
+    }
+
+    //todo check necessity
     public String getIdentificativo() {
         return identificativo.toString();
     }
---
+++ api/src/test/java/it/unicam/ids/studenti/ll/model/UtenteConPrivilegiTest.java
@@ -0,0 +1,13 @@
+package it.unicam.ids.studenti.ll.model;
+
+import org.junit.jupiter.api.Test;
+
+public class UtenteConPrivilegiTest {
+    @Test
+    void addPrivilegiTest(){
+        Dipendente d = new Dipendente("Mario", "Bazzuzzi");
+        Commerciante c = new Commerciante("PMR");
+//        d.addPermesso(() -> c.addCliente());
+
+    }
+}
