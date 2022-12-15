d5e62e05adedd281a87fc6c8d420351176e45aeb
Glydric22 <leo@miglio.dev>
2022-12-15 19:12:16 +0100

    [minor]

--- api/src/main/java/it/unicam/ids/studenti/ll/model/UpdatableProgrammaFedelta.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/UpdatableProgrammaFedelta.java
@@ -1,13 +1,13 @@
 package it.unicam.ids.studenti.ll.model;
 
 public interface UpdatableProgrammaFedelta extends ProgrammaFedelta {
-	//TODO aggiungere dei Runnable per definire le regole di aggiornamento
+    //TODO aggiungere dei Runnable per definire le regole di aggiornamento
 
-	/**
-	 * Prende in input il costo del prodotto e calcola
-	 * gli aggiornamenti da effettuare
-	 * @param value il costo del prodotto
-	 */
-	void aggiornaProgramma(float value);
+    /**
+     * Prende in input il costo del prodotto e calcola
+     * gli aggiornamenti da effettuare
+     * @param value il costo del prodotto
+     */
+    void aggiornaProgramma(float value);
 
 }
