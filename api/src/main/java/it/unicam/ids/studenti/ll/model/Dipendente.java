ceabc216e9b266985a20bc1fa8dc39237a66d75b
Glydric22 <leo@miglio.dev>
2022-12-15 12:01:43 +0100

    [aggiunta] metodi di comodo

--- api/src/main/java/it/unicam/ids/studenti/ll/model/Dipendente.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/Dipendente.java
@@ -5,10 +5,15 @@ import java.time.LocalDate;
 class Dipendente extends UtenteConPrivilegi {
     protected Azienda lavoraIn;
 
+    protected Dipendente(String nome, String cognome) {
+        super(nome, cognome);
+    }
+
     public Dipendente(String nome, String cognome, LocalDate dataNascita) {
         super(nome, cognome, dataNascita);
     }
-    public Dipendente(String nome, String cognome, String identificativo,LocalDate dataNascita) {
+
+    public Dipendente(String nome, String cognome, String identificativo, LocalDate dataNascita) {
         super(nome, cognome, identificativo, dataNascita);
     }
 }
