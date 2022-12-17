681f588b7f278bdba4cc8518257ee8cb6851e426
Glydric22 <leo@miglio.dev>
2022-12-17 20:05:11 +0100

    [add] methods to handle azienda

--- api/src/main/java/it/unicam/ids/studenti/ll/model/Dipendente.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/Dipendente.java
@@ -2,18 +2,34 @@ package it.unicam.ids.studenti.ll.model;
 
 import java.time.LocalDate;
 
-class Dipendente extends UtenteConPrivilegi {
+public class Dipendente extends UtenteConPrivilegi {
     protected Azienda lavoraIn;
 
     protected Dipendente(String nome, String cognome) {
         super(nome, cognome);
     }
 
-    public Dipendente(String nome, String cognome, LocalDate dataNascita) {
+    public Dipendente(String nome, String cognome, LocalDate dataNascita, Azienda azienda) {
         super(nome, cognome, dataNascita);
+        assumiIn(azienda);
     }
 
     public Dipendente(String nome, String cognome, LocalDate dataNascita, String identificativo) {
         super(nome, cognome, dataNascita,identificativo);
+    }
+
+    public Azienda getAzienda() {
+        return lavoraIn;
+    }
+
+    /**
+     *
+     * @param azienda non nulla
+     */
+    public void assumiIn(Azienda azienda) {
+        if (azienda == null)
+            throw new IllegalArgumentException("Un proprietario non può essere istaziato senza azienda, manca di ragione");
+        azienda.addDipendente(this);
+        this.lavoraIn = azienda;
     }
 }
