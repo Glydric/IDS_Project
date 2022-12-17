71e309ad6d97d4d2341113c52ea45d6f803fbaaf
Glydric22 <leo@miglio.dev>
2022-12-17 10:35:43 +0100

    [add tests]

--- app/src/test/java/it/unicam/ids/studenti/ll/app/BackofficeTest.java
+++ app/src/test/java/it/unicam/ids/studenti/ll/app/BackofficeTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class BackofficeTest {
     @Test
     void loginProprietarioTest() {
@@ -13,6 +15,7 @@ public class BackofficeTest {
         p.setPassword("bimario");
         Commerciante c = new Commerciante("xbox", LocalDate.MIN, p);
 
+        assertThrows(IllegalArgumentException.class,()->new Backoffice(p.identificativo,"wrongPassword"));
         Backoffice b = new Backoffice(p.identificativo, "bimario");
 
     }
