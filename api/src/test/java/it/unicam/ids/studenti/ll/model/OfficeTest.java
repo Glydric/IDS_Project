5c59bec23ed4b5e86a374e8287d290e9b3e5a5ff
Glydric <leo@miglio.dev>
2023-01-12 18:33:14 +0100

    [add] cliente capability

--- api/src/test/java/it/unicam/ids/studenti/ll/model/OfficeTest.java
+++ api/src/test/java/it/unicam/ids/studenti/ll/model/OfficeTest.java
@@ -1,4 +1,4 @@
-package it.unicam.ids.studenti.ll.controller;
+package it.unicam.ids.studenti.ll.model;
 
 import it.unicam.ids.studenti.ll.model.*;
 import org.junit.jupiter.api.Test;
@@ -14,21 +14,44 @@ public class OfficeTest {
     Proprietario proprietario = new Proprietario("Mariooo", "Mazzolini", LocalDate.of(1990, 10, 25), azienda);
 
     @Test
-    void addDipendentePermissionTest() throws AuthorizationException {
+    void permissionTest() throws AuthorizationException {
         Persona persona = new Persona("Mocassini", "Luigini", LocalDate.of(1958, 2, 15));
 
         azienda.addDipendente(persona);
         Dipendente d = (Dipendente) Identificatore.getUtenteFrom("Mocassini.Luigini");
 
         Office frontOffice = new Office(d.identificativo, "");
-        Office backOffice = new Office(proprietario.identificativo, "");
 
         Persona p2 = new Persona("Macciccio", "Luigone", LocalDate.of(1958, 2, 15));
 
         assertThrows(AuthorizationException.class, () -> frontOffice.aggiungiDipendente(p2));
 
-        backOffice.allowDipendente(d, "aggiungiDipendente");
+        new Office(proprietario.identificativo, "").allowDipendente(d, "aggiungiDipendente");
 
         assertDoesNotThrow(() -> frontOffice.aggiungiDipendente(p2));
+    }
+
+    @Test
+    void aggiungiDipendentePermissionTest() {
+        Persona persona = new Persona("Mocassini", "Luigini", LocalDate.of(1958, 2, 15));
+
+        Office backOffice = new Office(proprietario.identificativo, "");
+
+        assertThrows(AuthorizationException.class, () -> backOffice.aggiungiDipendente(persona));
+    }
+
+    @Test
+    void aggiungiClienteTest() {
+        Cliente cliente = new Cliente(
+                "Mocassini",
+                "Luigini",
+                LocalDate.of(1958, 2, 15),
+                "4489275849",
+                "bella@pe.te"
+        );
+
+        Office backOffice = new Office(proprietario.identificativo, "");
+
+        assertThrows(AuthorizationException.class, () -> backOffice.aggiungiCliente(cliente));
     }
 }
