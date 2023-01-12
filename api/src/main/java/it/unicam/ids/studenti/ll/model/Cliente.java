c253d0c8b728703d1c11da27881e807e72fd4e2c
Glydric <leo@miglio.dev>
2023-01-12 17:58:00 +0100

    [add UUID]

--- api/src/main/java/it/unicam/ids/studenti/ll/model/Cliente.java
+++ api/src/main/java/it/unicam/ids/studenti/ll/model/Cliente.java
@@ -3,13 +3,12 @@ package it.unicam.ids.studenti.ll.model;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Cliente extends Persona {
-    public int identificativoTessera;
-    //String è meglio secondo me, perchè Integer è grande 10 cifre (2147483647) e serve per farci i calcoli.
-
+    public UUID identificativoTessera;
     public Set<Coalizione> listaCommercianti = new HashSet<>();
     public boolean isFamily = false;
     private String numeroTelefono;
@@ -21,23 +20,19 @@ public class Cliente extends Persona {
      */
     protected Cliente(String nome, String cognome) {
         super(nome, cognome);
-        identificativoTessera = 0;
+        this.identificativoTessera = UUID.randomUUID();
     }
 
     public Cliente(String nome, String cognome, LocalDate dataNascita, String numeroTelefono, String email, boolean isFamily) {
         this(nome, cognome, dataNascita, numeroTelefono, email);
         this.isFamily = isFamily;
-        // todo identificativo Tessera deve essere un ID, come lo creiamo?
-        //  random UUID?
     }
 
     public Cliente(String nome, String cognome, LocalDate dataNascita, String numeroTelefono, String email) {
         super(nome, cognome, dataNascita);
         setNumeroTelefono(numeroTelefono);
         setEmail(email);
-        this.identificativoTessera = 0;
-        // todo identificativo Tessera deve essere un ID, come lo creiamo?
-        //  random UUID?
+        this.identificativoTessera = UUID.randomUUID();
     }
 
     public String getEmail() {
@@ -53,10 +48,9 @@ public class Cliente extends Persona {
     }
 
     public void setNumeroTelefono(String numeroTelefono) {
-        // todo fatto
         Pattern numTelx = Pattern.compile("[0-9]{10}");
         Matcher controllore = numTelx.matcher(numeroTelefono);
-        if (!controllore.find()){
+        if (!controllore.find()) {
             throw new IllegalArgumentException("Numero non valida");
         }
         this.numeroTelefono = numeroTelefono;
