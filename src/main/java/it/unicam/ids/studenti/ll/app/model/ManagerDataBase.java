446ad7ec2a429a26b1c2a0df784963c692c02a9f
Glydric <leo@miglio.dev>
2023-01-24 20:48:10 +0100

    fixed bug

--- src/main/java/it/unicam/ids/studenti/ll/app/model/ManagerDataBase.java
+++ src/main/java/it/unicam/ids/studenti/ll/app/model/ManagerDataBase.java
@@ -3,28 +3,42 @@ package it.unicam.ids.studenti.ll.app.model;
 
 //TODO salvare dati dei commercianti, coalizione e dei clienti..le tre C.
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ManagerDataBase {
+    private static ManagerDataBase instance;
+    private String url, user, pwd;
+    private Connection conn;
 
-    public void salva (Commerciante commerciante){
+    private void connect() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, pwd);
+            System.out.println("Database connected , ready to go!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your PostgreSQL JDBC Driver? " + "Include in your library path!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Problems in opening a connection to the DB");
+            e.printStackTrace();
+        }
+    }
+
+    public void salva(Commerciante commerciante) {
+        connect();
+    }
+
+    public void salva(Cliente cliente) {
+        connect();
 
     }
 
-    public void salva (Cliente cliente){
+    public void salva(Coalizione coalizione) {
+        connect();
 
     }
-
-    public void salva (Coalizione coalizione){
-
-    }
-
-
-
-
-
-
-
-
-
 
 
 }
