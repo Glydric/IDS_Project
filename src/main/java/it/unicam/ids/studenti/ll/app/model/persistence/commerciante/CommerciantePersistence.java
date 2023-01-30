02b5b2eeb6060732a22893dea56e89289ab999c8
Glydric <leo@miglio.dev>
2023-01-30 11:40:38 +0100

    add methods to interact DB

--- src/main/java/it/unicam/ids/studenti/ll/app/model/persistence/commerciante/CommerciantePersistence.java
+++ src/main/java/it/unicam/ids/studenti/ll/app/model/persistence/commerciante/CommerciantePersistence.java
@@ -1,33 +1,73 @@
 package it.unicam.ids.studenti.ll.app.model.persistence.commerciante;
 
+import it.unicam.ids.studenti.ll.app.model.Commerciante;
+import it.unicam.ids.studenti.ll.app.model.ModelMapperConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 public class CommerciantePersistence {
     @Autowired
     private CommercianteRepository commercianteRepository;
-    public List<CommercianteEntity> getAllCommerciante(){
+
+    public List<CommercianteEntity> getAllCommerciante() {
         List<CommercianteEntity> commercianteEntitys = new ArrayList<>();
         commercianteRepository.findAll().forEach(commercianteEntitys::add);
         return commercianteEntitys;
 
     }
 
-    public void addCommerciante(CommercianteEntity commercianteEntity){
-        commercianteRepository.save(commercianteEntity);
+    public void updateCommerciante(Commerciante commerciante) {
+        CommercianteEntity entity =
+                ModelMapperConfig.mapper().map(
+                        commerciante,
+                        CommercianteEntity.class
+                );
+        commercianteRepository.update(
+                entity.getRagioneSociale(),
+                entity.getLinkEsterno(),
+                entity.getWantCoalize(),
+                entity.getCoalizione()
+        );
+    }
 
+    public void addCommerciante(Commerciante commerciante) throws IllegalArgumentException {
+        CommercianteEntity entity =
+                ModelMapperConfig.mapper().map(
+                        commerciante,
+                        CommercianteEntity.class
+                );
+        if (exists(entity.getRagioneSociale())) {
+            updateCommerciante(commerciante);
+            throw new IllegalArgumentException("Commerciante già esistente");
+        }
+
+        commercianteRepository.save(entity);
     }
-    public Optional<CommercianteEntity> getCommerciante(String ragioneSociale){
-        return commercianteRepository.findById(ragioneSociale);
+
+    public Commerciante getCommerciante(String ragioneSociale) {
+        Optional<CommercianteEntity> opt = commercianteRepository.findById(ragioneSociale);
+        if (opt.isEmpty())
+            throw new NoSuchElementException("Il commerciante non esiste");
+        return ModelMapperConfig
+                .mapper()
+                .map(
+                        opt.get(),
+                        Commerciante.class
+                );
     }
-    public void deleteCommerciante(String ragioneSociale){
+
+    public void deleteCommerciante(String ragioneSociale) {
         commercianteRepository.deleteById(ragioneSociale);
+    }
 
+    public boolean exists(String ragioneSociale) {
+        return commercianteRepository.findById(ragioneSociale).isPresent();
     }
 }
 
